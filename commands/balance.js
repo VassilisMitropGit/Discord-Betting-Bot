87f3aa8b3e3077583593ef3fa1435cbf5fb88b13
GitZonk <vassilis_mitropoulos@yahoo.gr>
2021-04-06 12:26:44 +0300

    Balance and daily commands now store data and use the Database.

--- commands/balance.js
+++ commands/balance.js
@@ -1,14 +1,13 @@
-// const fs = require("fs");
-// const money = require("../money.json");
 const mongoose = require("mongoose");
 const botconfig = require("../botconfig.json");
 
-//connect to database
+//Connect to the database
 mongoose.connect(botconfig.mongoPass, {
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
-//models
+//Models
 const Data = require("../models/data.js");
 
 module.exports.run = async (bot, message, args) => {
@@ -31,29 +30,11 @@ module.exports.run = async (bot, message, args) => {
                 daily: 0
             })
             newData.save().catch(err => console.log(err));
-            return message.channel.send(`${bot.users.cache.get(user.id).username} has $0.`);
+            return message.channel.send(`${bot.users.cache.get(user.id).username} has $0. Gotta start the grind.`);
         } else{
-            return message.channel.send(`${bot.users.cache.get(user.id).username} has $${data.money}.`);
+            return message.channel.send(`${bot.users.cache.get(user.id).username} has $${data.money}. SHEEEEEEEEEESH!`);
         }
     })
-
-    // if(!money[user.id]){
-    //     money[user.id] = {
-    //         name: bot.users.cache.get(user.id).tag,
-    //         money: 0,
-    //         id: user.id
-    //     }
-    //     fs.writeFile("./money.json", JSON.stringify(money), (err) => {
-    //         if(err) console.log(err);
-    //     });
-    // } else{
-    //     money[user.id].name = bot.users.cache.get(user.id).tag;
-    //     fs.writeFile("./money.json", JSON.stringify(money), (err) => {
-    //         if(err) console.log(err);
-    //     });
-    // }
-
-    // return message.channel.send(`${bot.users.cache.get(user.id).username} has $${money[user.id].money}.`);
 }
 
 module.exports.help = {
--- commands/daily.js
+++ commands/daily.js
@@ -1,86 +1,49 @@
 const Discord = require("discord.js");
-const fs = require("fs");
-const money = require("../money.json");
 const ms = require("parse-ms");
-const cooldowns = require("../cooldowns.json");
+const mongoose = require("mongoose");
+const botconfig = require("../botconfig.json");
+
+//Connect to the database
+mongoose.connect(botconfig.mongoPass, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+//Models
+const Data = require("../models/data.js");
 
 module.exports.run = async (bot, message, args) => {
     let timeout = 86400000;
     let reward = 100;
-    let embed = new Discord.MessageEmbed();
-    embed.setTitle("Daily Reward!");
 
-    if(!money[message.author.id]){
-        money[message.author.id] = {
-            name: bot.users.cache.get(message.author.id).tag,
-            money: reward
-        }
-        fs.writeFile("./money.json", JSON.stringify(money), (err) => {
-            if(err) console.log(err);
-        });
-
-        if(!cooldowns[message.author.id]){
-            cooldowns[message.author.id] = {
-                name: bot.users.cache.get(message.author.id).tag,
+    Data.findOne({
+        userID: message.author.id
+    }, (err, data) => {
+        if (err) console.log(err);
+        if (!data) {
+            const newData = new Data({
+                name: message.author.username,
+                userID: message.author.id,
+                lb: "all",
+                money: reward,
                 daily: Date.now()
-            }
-            fs.writeFile("./cooldowns.json", JSON.stringify(cooldowns), (err) => {
-                if(err) console.log(err);
-            });
-        } else {
-            cooldowns[message.author.id].daily = Date.now();
-            fs.writeFile("./cooldowns.json", JSON.stringify(cooldowns), (err) => {
-                if(err) console.log(err);
-            });
-        }
+            })
+            newData.save().catch(err => console.log(err));
+            return message.channel.send(`${message.author.username} has $${reward}.`);
+        } else{
+            if(timeout - (Date.now() - data.daily) > 0){
+                let time = ms(timeout - (Date.now() - data.daily));
 
-        embed.setDescription(`You collected your daily reward of ${reward}. Current balance is ${money[message.author.id].money}.`);
-        embed.setColor("00ff00");
-        return message.channel.send(embed);
-    } else {
-        if(!cooldowns[message.author.id]){
-            cooldowns[message.author.id] = {
-                name: bot.users.cache.get(message.author.id).tag,
-                daily: Date.now()
-            }
-            fs.writeFile("./cooldowns.json", JSON.stringify(cooldowns), (err) => {
-                if(err) console.log(err);
-            });
+                return message.reply(`You already collected your reward playa! Collect again in ${time.hours}h ${time.minutes}m ${time.seconds}s`);
+            } else{
+                data.money += reward;
+                data.daily = Date.now();
+                data.save().catch(err => console.log(err));
 
-            money[message.author.id].money += reward;
-            fs.writeFile("./money.json", JSON.stringify(money), (err) => {
-                if(err) console.log(err);
-            });
-
-            embed.setDescription(`You collected your daily reward of ${reward}. Current balance is ${money[message.author.id].money}.`);
-            embed.setColor("00ff00");
-            return message.channel.send(embed);
-
-        } else {
-            if(timeout - (Date.now() - cooldowns[message.author.id].daily) > 0){
-                let time = ms(timeout - (Date.now() - cooldowns[message.author.id].daily));
-
-                embed.setColor("ff0000");
-                embed.setDescription("You already collected your reward");
-                embed.addField(`Collect again in`, `${time.hours}h ${time.minutes}m ${time.seconds}s`);
-                return message.channel.send(embed);
-            } else {
-                money[message.author.id].money += reward;
-                fs.writeFile("./money.json", JSON.stringify(money), (err) => {
-                    if(err) console.log(err);
-                });
-
-                cooldowns[message.author.id].daily = Date.now();
-                fs.writeFile("./cooldowns.json", JSON.stringify(cooldowns), (err) => {
-                    if(err) console.log(err);
-                });
-
-                embed.setDescription(`You collected your daily reward of ${reward}. Current balance is ${money[message.author.id].money}.`);
-                embed.setColor("00ff00");
-                return message.channel.send(embed);
+                return message.reply(`You collected your daily reward of $${reward}. Current balance is $${data.money}.`);
             }
         }
-    }
+    })
 }
 
 module.exports.help = {
