5bf9e14a8741d1ed93c762f2e0ef14bc8607234d
GitZonk <vassilis_mitropoulos@yahoo.gr>
2021-04-06 11:40:46 +0300

    changes to gitigonre and database connection to the "balance" command

--- commands/balance.js
+++ commands/balance.js
@@ -1,5 +1,15 @@
-const fs = require("fs");
-const money = require("../money.json");
+// const fs = require("fs");
+// const money = require("../money.json");
+const mongoose = require("mongoose");
+const botconfig = require("../botconfig.json");
+
+//connect to database
+mongoose.connect(botconfig.mongoPass, {
+    useNewUrlParser: true
+});
+
+//models
+const Data = require("../models/data.js");
 
 module.exports.run = async (bot, message, args) => {
     if(!args[0]){
@@ -8,23 +18,42 @@ module.exports.run = async (bot, message, args) => {
         var user = message.mentions.users.first() || bot.users.cache.get(args[0]);
     }
 
-    if(!money[user.id]){
-        money[user.id] = {
-            name: bot.users.cache.get(user.id).tag,
-            money: 0,
-            id: user.id
+    Data.findOne({
+        userID: user.id
+    }, (err, data) => {
+        if (err) console.log(err);
+        if (!data) {
+            const newData = new Data({
+                name: bot.users.cache.get(user.id).username,
+                userID: user.id,
+                lb: "all",
+                money: 0,
+                daily: 0
+            })
+            newData.save().catch(err => console.log(err));
+            return message.channel.send(`${bot.users.cache.get(user.id).username} has $0.`);
+        } else{
+            return message.channel.send(`${bot.users.cache.get(user.id).username} has $${data.money}.`);
         }
-        fs.writeFile("./money.json", JSON.stringify(money), (err) => {
-            if(err) console.log(err);
-        });
-    } else{
-        money[user.id].name = bot.users.cache.get(user.id).tag;
-        fs.writeFile("./money.json", JSON.stringify(money), (err) => {
-            if(err) console.log(err);
-        });
-    }
+    })
 
-    return message.channel.send(`${bot.users.cache.get(user.id).username} has $${money[user.id].money}.`);
+    // if(!money[user.id]){
+    //     money[user.id] = {
+    //         name: bot.users.cache.get(user.id).tag,
+    //         money: 0,
+    //         id: user.id
+    //     }
+    //     fs.writeFile("./money.json", JSON.stringify(money), (err) => {
+    //         if(err) console.log(err);
+    //     });
+    // } else{
+    //     money[user.id].name = bot.users.cache.get(user.id).tag;
+    //     fs.writeFile("./money.json", JSON.stringify(money), (err) => {
+    //         if(err) console.log(err);
+    //     });
+    // }
+
+    // return message.channel.send(`${bot.users.cache.get(user.id).username} has $${money[user.id].money}.`);
 }
 
 module.exports.help = {
